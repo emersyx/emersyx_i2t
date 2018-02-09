07af733a56a8eb84cbf023ad3fd9c12546b00e36
Alexandru Geana <alex@alegen.net>
2018-02-09 23:01:50 +0100

    add unit test for toml configuration feature

---
+++ emi2t/conf_test.go
@@ -0,0 +1,42 @@
+package emi2t
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"testing"
+)
+
+var confFile = flag.String("conffile", "", "path to sample configuration fileto be used for testing purposes")
+
+func TestMain(m *testing.M) {
+	os.Exit(m.Run())
+}
+
+func TestParsing(t *testing.T) {
+	var cfg i2tConfig
+	loadConfig(*confFile, &cfg)
+
+	if len(cfg.Links) != 1 {
+		t.Log(fmt.Sprintf("expected 1 link in the config, got %d instead", len(cfg.Links)))
+		t.Fail()
+	}
+
+	link := cfg.Links[0]
+	if link.IRCGatewayID != "example_irc_id" {
+		t.Log(fmt.Sprintf("invalid IRC gateway ID, got \"%s\"", link.IRCGatewayID))
+		t.Fail()
+	}
+	if link.IRCChannel != "#emersyx" {
+		t.Log(fmt.Sprintf("invalid IRC channel, got \"%s\"", link.IRCChannel))
+		t.Fail()
+	}
+	if link.TelegramGatewayID != "example_telegram_id" {
+		t.Log(fmt.Sprintf("invalid Telegram gateway ID, got \"%s\"", link.TelegramGatewayID))
+		t.Fail()
+	}
+	if link.TelegramGroup != "@testingemersyx" {
+		t.Log(fmt.Sprintf("invalid Telegram group, got \"%s\"", link.TelegramGroup))
+		t.Fail()
+	}
+}
